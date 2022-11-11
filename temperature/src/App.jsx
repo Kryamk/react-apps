ac97c376835dc71ec030c06bfb6b40868cd46bb7
Ivan <kryamky@yandex.ru>
2022-11-11 12:34:36 +0300

    Add temperature calculator

---
+++ temperature/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Calculator from './components/CalculatorTemperature';
+import Test from './components/Test'
+
+function App() {
+	return (
+		<div className="App">
+			<h1>App Default</h1>
+			<Test/>
+			<Calculator/>
+		</div>
+	);
+}
+
+export default App;
